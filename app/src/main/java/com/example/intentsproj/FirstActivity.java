3525293bacdcda6876739b5cd7fc2159a960eaab
jayani0323 <jayanisaumya1998@gmail.com>
2020-08-14 19:34:31 +0530

    Tutorial 3

---
+++ app/src/main/java/com/example/intentsproj/FirstActivity.java
@@ -0,0 +1,23 @@
+package com.example.intentsproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class FirstActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+
+    }
+}
